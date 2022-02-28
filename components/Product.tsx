782d4c14b4f8b023a57a85c2db540d0166e05f06
Kondziii <1999konrad@gmail.com>
2022-02-28 19:24:00 +0100

    Styling list of products

--- components/Product.tsx
+++ components/Product.tsx
@@ -17,7 +17,7 @@ interface RatingProps {
 
 const Raiting = ({ rating }: RatingProps) => {
   return (
-    <p className='absolute text-xl font-bold text-yellow-500 top-4 right-6'>
+    <p className='absolute text-xl font-bold text-yellow-500 top-4 right-4'>
       {rating}
     </p>
   );
@@ -25,17 +25,19 @@ const Raiting = ({ rating }: RatingProps) => {
 
 export const Product = ({ data }: ProductProps) => {
   return (
-    <div className='relative flex flex-col justify-center h-full p-4 transition-all duration-300 bg-white rounded-lg shadow-lg align-center hover:shadow-xl'>
+    <div className='relative flex flex-col justify-center w-full h-full p-4 transition-all duration-300 bg-white rounded-lg shadow-lg align-center hover:shadow-2xl hover:scale-105'>
       <Image
-        width={160}
-        height={160}
+        width={250}
+        height={250}
         className='object-contain text-center aspect-video'
         src={data.thumbnailSrc}
         alt={data.thumbnailAlt}
       />
-      <h2 className='mt-4 mb-2 text-lg font-medium '>{data.title}</h2>
-      <Raiting rating={data.rating} />
-      <p className='font-light text-gray-600 truncate'>{data.description}</p>
+      <div className='px-4'>
+        <h2 className='mt-4 mb-2 text-lg font-medium '>{data.title}</h2>
+        <Raiting rating={data.rating} />
+        <p className='font-light text-gray-600 truncate'>{data.description}</p>
+      </div>
     </div>
   );
 };
--- pages/products.tsx
+++ pages/products.tsx
@@ -6,7 +6,7 @@ const ProductsPage = ({
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div>
-      <ul className='grid gap-4 sm:gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+      <ul className='grid grid-cols-1 gap-4 sm:gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
         {products.map((product) => (
           <li key={product.id}>
             <Product
