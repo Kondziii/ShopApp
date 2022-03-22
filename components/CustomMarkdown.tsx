f76e2e8a4e2d727fe66eca213d7469e19aff5007
Kondziii <1999konrad@gmail.com>
2022-03-22 12:30:02 +0100

    Product list item adjusted to icons change v2

--- components/CustomMarkdown.tsx
+++ components/CustomMarkdown.tsx
@@ -11,8 +11,8 @@ const CustomMarkdown = ({ children }: { children: MarkdownResult }) => {
           if (!href) return <a {...props}></a>;
 
           if (
-            process.env.NEXT_PUBLIC_DOMAIN?.split('/').at(2) !==
-            href.split('/').at(2)
+            process.env.NEXT_PUBLIC_DOMAIN?.split('/')[2] !==
+            href.toString().split('/')[2]
           )
             return <a {...props} rel='noopener noreferrer'></a>;
 
