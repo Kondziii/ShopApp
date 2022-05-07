b5e270df82e6e27581822b3b450c7d9e61f4fd49
Kondziii <1999konrad@gmail.com>
2022-05-07 11:09:29 +0200

    Newsletter bug fixed

--- hooks/useAddToNewsletterMutation.tsx
+++ hooks/useAddToNewsletterMutation.tsx
@@ -2,7 +2,7 @@ import { useMutation } from 'react-query';
 
 export const useAddToNewsletterMutation = () => {
   return useMutation('add-to-newsletter', async (email: string) => {
-    await fetch('http://localhost:3000/api/newsletter', {
+    await fetch('/api/newsletter', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email: email }),
