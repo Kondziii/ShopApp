b6f482a142b2b65684a5611e5599c49951eb08bd
Kondziii <1999konrad@gmail.com>
2022-05-07 15:37:28 +0200

    Logo responsivness fixed

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,13 +5,17 @@ import React from 'react';
 export const Logo = (props: React.LinkHTMLAttributes<HTMLAnchorElement>) => {
   return (
     <Link href='/' passHref>
-      <Image
-        className='cursor-pointer'
-        src='/Logo.png'
-        alt='Stoopki logo'
-        width={180}
-        height={50}
-      />
+      <div className='h-12 w-32 sm:w-40'>
+        <Image
+          className='cursor-pointer'
+          src='/Logo.png'
+          alt='Stoopki logo'
+          width={8}
+          height={3}
+          layout='responsive'
+          objectFit='contain'
+        />
+      </div>
     </Link>
   );
 };
